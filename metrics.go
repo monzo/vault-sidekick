8d1457f29a0b4161a0dc9dbc3ac05c2eabd549eb
Sam Wood <sam@sam.uk.net>
2019-06-20 10:41:58 +0100

    Remove use of channels within the metrics collector struct
    There's no need to use channels in the metrics collector struct; when
    the metric reporting functions are called, we can update the metric
    storage maps straight away. At the moment, we're just pushing to a
    channel, and then receiving from it and immediately writing to the map -
    the channels serve no purpose.
    
    Also, remove the error label from the resource error metric, as we've
    decided there's no need to differentiate between get and renewal errors.

--- metrics.go
+++ metrics.go
@@ -15,131 +15,77 @@ type MetricsCollector struct {
 	resourceErrorsMetric  *prometheus.Desc
 	errorsMetric          *prometheus.Desc
 
-	resourceExpiryUpdates chan ResourceExpiry
+	// resourceExpiry is a map from resource ID to the latest expiry, as a duration from the current time.
 	resourceExpiry        map[string]time.Duration
 
-	resourceTotalUpdates chan ResourceTotal
-	resourceTotals       map[ResourceTotal]int
+	// resource{Totals,Successes,Errors} tracks counts of renewals per resource ID, and whether they succeeded or failed.
+	resourceTotals       map[string]int
+	resourceSuccesses      map[string]int
+	resourceErrors       map[string]int
 
-	resourceSuccessUpdates chan ResourceSuccess
-	resourceSuccesses      map[ResourceSuccess]int
-
-	resourceErrorUpdates chan ResourceError
-	resourceErrors       map[ResourceError]int
-
-	errorUpdates chan Error
-	errors       map[Error]int
+	// errors Tracks counts generic, non-resource related errors, by reason.
+	errors       map[string]int
 
 	metricsMutex sync.RWMutex
-}
-
-type Error struct {
-	err string
-}
-
-type ResourceExpiry struct {
-	resourceID string
-	expiresIn time.Duration
-}
-
-type ResourceTotal struct {
-	resourceID string
-}
-
-type ResourceSuccess struct {
-	resourceID string
-}
-
-type ResourceError struct {
-	resourceID string
-	err        string
 }
 
 var metrics *MetricsCollector
 var metricsMutex sync.Mutex
 
-func (m MetricsCollector) init() {
-	for {
-		select {
-		case resourceExpiry := <-m.resourceExpiryUpdates:
-			m.metricsMutex.Lock()
-			m.resourceExpiry[resourceExpiry.resourceID] = resourceExpiry.expiresIn
-			m.metricsMutex.Unlock()
-		case resourceTotal := <-m.resourceTotalUpdates:
-			m.metricsMutex.Lock()
-			m.resourceTotals[resourceTotal]++
-			m.metricsMutex.Unlock()
-		case resourceSuccess := <-m.resourceSuccessUpdates:
-			m.metricsMutex.Lock()
-			m.resourceSuccesses[resourceSuccess]++
-			m.metricsMutex.Unlock()
-		case resourceErr := <-m.resourceErrorUpdates:
-			m.metricsMutex.Lock()
-			m.resourceErrors[resourceErr]++
-			m.metricsMutex.Unlock()
-		case err := <-m.errorUpdates:
-			m.metricsMutex.Lock()
-			m.errors[err]++
-			m.metricsMutex.Unlock()
-		}
-	}
-}
-
-func (m *MetricsCollector) Error(err string) {
-	m.errorUpdates <- Error{
-		err: err,
-	}
+func (m *MetricsCollector) Error(reason string) {
+	m.metricsMutex.Lock()
+	m.errors[reason]++
+	m.metricsMutex.Unlock()
 }
 
 func (m *MetricsCollector) ResourceExpiry(resourceID string, expiresIn time.Duration) {
-	m.resourceExpiryUpdates<- ResourceExpiry {
-		resourceID: resourceID,
-		expiresIn: expiresIn,
-	}
+	m.metricsMutex.Lock()
+	m.resourceExpiry[resourceID] = expiresIn
+	m.metricsMutex.Unlock()
 }
 
 func (m *MetricsCollector) ResourceTotal(resourceID string) {
-	m.resourceTotalUpdates <- ResourceTotal{
-		resourceID: resourceID,
-	}
+	m.metricsMutex.Lock()
+	m.resourceTotals[resourceID]++
+	m.metricsMutex.Unlock()
 }
 
 func (m *MetricsCollector) ResourceSuccess(resourceID string) {
-	m.resourceSuccessUpdates <- ResourceSuccess{
-		resourceID: resourceID,
-	}
+	m.metricsMutex.Lock()
+	m.resourceSuccesses[resourceID]++
+	m.metricsMutex.Unlock()
 }
 
-func (m *MetricsCollector) ResourceError(resourceID, err string) {
-	m.resourceErrorUpdates <- ResourceError{
-		resourceID: resourceID,
-		err:        err,
-	}
+func (m *MetricsCollector) ResourceError(resourceID string) {
+	m.metricsMutex.Lock()
+	m.resourceErrors[resourceID]++
+	m.metricsMutex.Unlock()
 }
 
 func RegisterMetricsCollector(role string) {
 	metricsMutex.Lock()
 	defer metricsMutex.Unlock()
 
+	resourceAndRoleLabels := []string{"resource_id", "role"}
 	metrics = &MetricsCollector{
 		resourceExpiryMetric: prometheus.NewDesc("vault_sidekick_certificate_expiry_gauge",
 			"vault_sidekick_certificate_expiry_gauge",
-			[]string{"resource_id", "role"},
+			resourceAndRoleLabels,
 			nil,
 		),
 		resourceTotalMetric: prometheus.NewDesc("vault_sidekick_resource_total_counter",
 			"vault_sidekick_resource_total_counter",
-			[]string{"resource", "role"},
+			resourceAndRoleLabels,
 			nil,
 		),
 		resourceSuccessMetric: prometheus.NewDesc("vault_sidekick_resource_success_counter",
 			"vault_sidekick_resource_success_counter",
-			[]string{"resource", "role"},
+			resourceAndRoleLabels,
 			nil,
 		),
 		resourceErrorsMetric: prometheus.NewDesc("vault_sidekick_resource_error_counter",
 			"vault_sidekick_resource_error_counter",
-			[]string{"resource", "error", "role"},
+			resourceAndRoleLabels,
 			nil,
 		),
 		errorsMetric: prometheus.NewDesc("vault_sidekick_error_counter",
@@ -150,23 +96,14 @@ func RegisterMetricsCollector(role string) {
 
 		role: role,
 
-		resourceExpiryUpdates: make(chan ResourceExpiry, 10),
 		resourceExpiry:        make(map[string]time.Duration),
 
-		resourceTotalUpdates: make(chan ResourceTotal, 10),
-		resourceTotals:      make(map[ResourceTotal]int),
+		resourceTotals:      make(map[string]int),
+		resourceSuccesses:      make(map[string]int),
+		resourceErrors:       make(map[string]int),
 
-		resourceSuccessUpdates: make(chan ResourceSuccess, 10),
-		resourceSuccesses:      make(map[ResourceSuccess]int),
-
-		resourceErrorUpdates: make(chan ResourceError, 10),
-		resourceErrors:       make(map[ResourceError]int),
-
-		errorUpdates: make(chan Error, 10),
-		errors:       make(map[Error]int),
+		errors:       make(map[string]int),
 	}
-
-	go metrics.init()
 
 	prometheus.MustRegister(metrics)
 }
@@ -195,23 +132,23 @@ func (m *MetricsCollector) Collect(ch chan<- prometheus.Metric) {
 			resourceID, m.role)
 	}
 
-	for resourceTotal, totalCount := range m.resourceTotals{
+	for resourceID, totalCount := range m.resourceTotals{
 		ch <- prometheus.MustNewConstMetric(m.resourceTotalMetric, prometheus.CounterValue, float64(totalCount),
-			resourceTotal.resourceID, m.role)
+			resourceID, m.role)
 	}
 
-	for resourceSuccess, successCount := range m.resourceSuccesses {
+	for resourceID, successCount := range m.resourceSuccesses {
 		ch <- prometheus.MustNewConstMetric(m.resourceSuccessMetric, prometheus.CounterValue, float64(successCount),
-			resourceSuccess.resourceID, m.role)
+			resourceID, m.role)
 	}
 
-	for resourceErr, errCount := range m.resourceErrors {
+	for resourceID, errCount := range m.resourceErrors {
 		ch <- prometheus.MustNewConstMetric(m.resourceErrorsMetric, prometheus.CounterValue, float64(errCount),
-			resourceErr.resourceID, resourceErr.err, m.role)
+			resourceID, m.role)
 	}
 
-	for err, errCount := range m.errors {
+	for reason, errCount := range m.errors {
 		ch <- prometheus.MustNewConstMetric(m.errorsMetric, prometheus.CounterValue, float64(errCount),
-			err.err, m.role)
+			reason, m.role)
 	}
 }
